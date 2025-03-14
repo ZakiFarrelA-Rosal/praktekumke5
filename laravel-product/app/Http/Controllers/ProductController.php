923ba0c83ad7112515da71e1cb492b1713b0ede9
ZakiFarrelA-Rosal <zadinerosal.1610@gmail.com>
2025-03-14 11:17:16 +0700

    initialcodes

---
+++ laravel-product/app/Http/Controllers/ProductController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::all();
+        return view('products.index', compact('products'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('products.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'stock' => 'required|numeric'
+        ]);
+    
+        $product = new Product($validatedData);
+        $product->save();
+    
+        return redirect()->route('products.index')->with('success', 'Product telah dibuat dengan sukses.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Product $product)
+    {
+        return view('products.show', compact('product'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Product $product)
+    {
+        return view('products.edit', compact('product'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Product $product)
+    {
+        $validatedData = $request->validate([
+            'name' => 'required|max:255',
+            'description' => 'required',
+            'price' => 'required|numeric',
+            'stock' => 'required|numeric'
+        ]);
+    
+        $product->update($validatedData);
+    
+        return redirect()->route('products.index')->with('success', 'Product telah diperbarui dengan sukses.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Product $product)
+    {
+        $product->delete();
+
+    return redirect()->route('products.index')->with('success', 'Product telah dihapus dengan sukses.');
+    }
+}
